2b0ffe121d30b9ce77132486889683d05ef127c6
yarik2486 <yarik2486@bk.ru>
2024-10-10 09:59:02 -0700

    Создан "Чек-лист для одобрения контрагента".

---
+++ OMYA.CounterpartyApproval/OMYA.CounterpartyApproval.ClientBase/Checklist/ChecklistHandlers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using OMYA.CounterpartyApproval.Checklist;
+
+namespace OMYA.CounterpartyApproval
+{
+  partial class ChecklistClientHandlers
+  {
+
+  }
+}
---
+++ OMYA.CounterpartyApproval/OMYA.CounterpartyApproval.Server/Checklist/ChecklistHandlers.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using OMYA.CounterpartyApproval.Checklist;
+
+namespace OMYA.CounterpartyApproval
+{
+}
