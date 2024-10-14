7a03ec463e8d162f4d6bad0a6f542b707849a3dc
yarik2486 <yarik2486@bk.ru>
2024-10-14 13:40:08 -0700

    Кнопка "Создать контрагента".

--- OMYA.CounterpartyApproval/OMYA.CounterpartyApproval.ClientBase/CounterpartyApprovalRequest/CounterpartyApprovalRequestActions.cs
+++ OMYA.CounterpartyApproval/OMYA.CounterpartyApproval.ClientBase/CounterpartyApprovalRequest/CounterpartyApprovalRequestActions.cs
@@ -9,6 +9,24 @@ namespace OMYA.CounterpartyApproval.Client
 {
   partial class CounterpartyApprovalRequestActions
   {
+    public virtual void CreateCompany(Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+      _obj.Save();
+      var company = Functions.CounterpartyApprovalRequest.Remote.CreateCompany(_obj);
+      company.ShowModal();
+      
+      if (!company.State.IsInserted)
+      {
+        _obj.Counterparty = company;
+        _obj.Save();
+      }
+    }
+
+    public virtual bool CanCreateCompany(Sungero.Domain.Client.CanExecuteActionArgs e)
+    {
+      return true;
+    }
+
     public virtual void CreateChecklist(Sungero.Domain.Client.ExecuteActionArgs e)
     {
       var checklist = Functions.CounterpartyApprovalRequest.Remote.CreateChecklist();
--- OMYA.CounterpartyApproval/OMYA.CounterpartyApproval.Server/CounterpartyApprovalRequest/CounterpartyApprovalRequestServerFunctions.cs
+++ OMYA.CounterpartyApproval/OMYA.CounterpartyApproval.Server/CounterpartyApprovalRequest/CounterpartyApprovalRequestServerFunctions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Sungero.Core;
 using Sungero.CoreEntities;
 using OMYA.CounterpartyApproval.CounterpartyApprovalRequest;
+using OMYA.CounterpartySolution;
 
 namespace OMYA.CounterpartyApproval.Server
 {
@@ -18,6 +19,46 @@ namespace OMYA.CounterpartyApproval.Server
     public static IChecklist CreateChecklist()
     {
       return Checklists.Create();
+    }
+    
+    /// <summary>
+    /// Создать контрагента и заполнить поля из заявки на одобрение контрагента.
+    /// </summary>
+    /// <returns>Контрагент.</returns>
+    [Remote]
+    public ICompany CreateCompany()
+    {
+      var company = Companies.Create();
+      company.Nonresident = _obj.Nonresident;
+      company.CounterpartyTypeOMYA = _obj.CounterpartyType;
+      company.CounterpartyKindOMYA = _obj.CounterpartyKind;
+      company.Name = _obj.ShortName;
+      company.LegalName = _obj.LegalName;
+      company.TIN = _obj.TIN;
+      company.TRRC = _obj.TRRC;
+      company.PSRN = _obj.PSRN;
+      company.NCEO = _obj.NCEO;
+      company.NCEA = _obj.NCEA;
+      company.SAPNumOMYA = _obj.SAPNum;
+      company.CompanyRegistrationNumberOMYA = _obj.CompanyRegistrationNumber;
+      company.TaxNumberOMYA = _obj.TaxNumber;
+      company.Region = _obj.Region;
+      company.City = _obj.City;
+      company.LegalAddress = _obj.LegalAddress;
+      company.PostalAddress = _obj.PostalAddress;
+      company.Phones = _obj.Phones;
+      company.Email = _obj.Email;
+      company.Homepage = _obj.Homepage;
+      company.Account = _obj.Account;
+      company.CurrentAccountOMYA = _obj.CurrentAccount;
+      company.Bank = _obj.Bank;
+      company.BankAddressOMYA = _obj.BankAddress;
+      company.CorrespondentAccountOMYA = _obj.CorrespondentAccount;
+      company.BICOMYA = _obj.BIC;
+      company.IBANOMYA = _obj.IBAN;
+      company.SWIFTOMYA = _obj.SWIFT;
+      
+      return company;
     }
   }
 }
